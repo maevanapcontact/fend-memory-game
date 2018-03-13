cbf40eeb73c6d5afb8a60eef812553762be60e81
Maeva NAP <maevanap.contact@gmail.com>
2018-03-13 13:21:42 +0100

    feat: add click event on the grid with delegation

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,12 @@ let gameType = "normal";
 let numberRound = 1;
 let currentRound = 1;
 
+// DOM elements to listen to
+const GRID = document.getElementById('grid');
+
 do {
-  
-} while (currentRound < numberRound);
+  GRID.addEventListener('click', function(event) {
+    // Click on a card
+    if (event.target.nodeName === 'ARTICLE') console.log('Event listener on grid');
+  });
+} while (currentRound < numberRound); // Continue until reach numberRound
